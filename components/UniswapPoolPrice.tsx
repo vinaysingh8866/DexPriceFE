dc1ecf6e610a643e8f233c2052dac64e83288ec8
Vinay Singh <vinaysingh8866@gmail.com>
2022-06-07 10:10:00 -0400

    Added Component to get token prices

--- components/UniswapPoolPrice.tsx
+++ components/UniswapPoolPrice.tsx
@@ -1,69 +1,96 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { Contract, ethers } from "ethers";
 import { Pool } from "@uniswap/v3-sdk";
-import { Currency, CurrencyAmount, Percent, Token, TradeType } from "@uniswap/sdk-core";
+import {
+  Currency,
+  CurrencyAmount,
+  Percent,
+  Token,
+  TradeType,
+} from "@uniswap/sdk-core";
 import { abi as IUniswapV3PoolABI } from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
 import { abi as IUniswapV3FactoryABI } from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Factory.sol/IUniswapV3Factory.json";
-
+import UniswapTokenPrice from "./UniswapTokenPrice";
 
 const abi = [
-    // Read-Only Functions
-    "function balanceOf(address owner) view returns (uint256)",
-    "function decimals() view returns (uint8)",
-    "function symbol() view returns (string)",
+  // Read-Only Functions
+  "function balanceOf(address owner) view returns (uint256)",
+  "function decimals() view returns (uint8)",
+  "function symbol() view returns (string)",
 
-    // Authenticated Functions
-    "function transfer(address to, uint amount) returns (bool)",
+  // Authenticated Functions
+  "function transfer(address to, uint amount) returns (bool)",
 
-    // Events
-    "event Transfer(address indexed from, address indexed to, uint amount)"
+  // Events
+  "event Transfer(address indexed from, address indexed to, uint amount)",
 ];
 
-function UniswapPoolPrice(props:any) {
-    const [got, setGot] = useState(false)
-    const [poolInfo, setPoolInfo] = useState({"poolAdd":"", "token1":"", "token2":""})
-    useEffect(()=>{
-        async function getPoolAddresses(){
-            try{
-                const factoryContractAddress = "0x1F98431c8aD98523631AE4a59f267346ea31F984"
-                const contractInstance = new Contract(factoryContractAddress, IUniswapV3FactoryABI, props.provider)
-                const owner = await contractInstance.owner()
-                const poolAddress = await contractInstance.getPool(props.token1, props.token2,3000)
-                const poolContract = new Contract(poolAddress, IUniswapV3PoolABI, props.provider)
-                const token0 = await poolContract.token0()
-                const token1 = await poolContract.token1()
-                const tok1C = new ethers.Contract(token0, abi, props.provider);
-                const tokSym1:string = await tok1C.symbol()
-                const tok2C = new ethers.Contract(token1, abi, props.provider);
-                const tokSym2:string = await tok2C.symbol()
-                const tempVal = {"poolAdd":poolAddress, "token1":tokSym1, "token2":tokSym2}
-                setPoolInfo(tempVal)
-                setGot(true)
-            }
-            catch{
-
-            }
-            
-        }
-        if(!got){
-            getPoolAddresses()
-        }
-        
-        
-    },[got, props.provider, props.token1, props.token2])
-    return (
+function UniswapPoolPrice(props: any) {
+  const [got, setGot] = useState(false);
+  const [poolInfo, setPoolInfo] = useState({
+    poolAdd: "",
+    token1: "",
+    token1Dec: "",
+    token2: "",
+    token2Dec: "",
+  });
+  useEffect(() => {
+    async function getPoolAddresses() {
+      try {
+        const factoryContractAddress =
+          "0x1F98431c8aD98523631AE4a59f267346ea31F984";
+        const contractInstance = new Contract(
+          factoryContractAddress,
+          IUniswapV3FactoryABI,
+          props.provider
+        );
+        const owner = await contractInstance.owner();
+        const poolAddress: string = await contractInstance.getPool(
+          props.token1,
+          props.token2,
+          3000
+        );
+        const poolContract = new Contract(
+          poolAddress,
+          IUniswapV3PoolABI,
+          props.provider
+        );
+        const token0 = await poolContract.token0();
+        const token1 = await poolContract.token1();
+        const tok1C = new ethers.Contract(token0, abi, props.provider);
+        const tokSym1: string = await tok1C.symbol();
+        const tok2C = new ethers.Contract(token1, abi, props.provider);
+        const tokSym2: string = await tok2C.symbol();
+        const tok1Dec: string = await tok1C.decimals();
+        const tok2Dec: string = await tok2C.decimals();
+        setPoolInfo({
+          poolAdd: poolAddress,
+          token1: tokSym1,
+          token1Dec: tok1Dec,
+          token2: tokSym2,
+          token2Dec: tok2Dec,
+        });
+        setGot(true);
+      } catch {}
+    }
+    if (!got) {
+      getPoolAddresses();
+    }
+  }, [got, props.provider, props.token1, props.token2]);
+  return (
+    <div>
+      {got ? (
         <div>
-        { got 
-            ? <div> {poolInfo.token1}  {poolInfo.token2}</div> 
-            : <div></div>
-        }
+          <UniswapTokenPrice
+            poolInfo={poolInfo}
+            provider={props.provider}
+          ></UniswapTokenPrice>
         </div>
-        
-        
-    );
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 }
-    
-
-
 
 export default UniswapPoolPrice;
---
+++ components/UniswapTokenPrice.tsx
@@ -0,0 +1,130 @@
+import { Token } from "@uniswap/sdk-core";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import { abi as IUniswapV3PoolABI } from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
+import { Pool } from "@uniswap/v3-sdk";
+interface Immutables {
+  factory: string;
+  token0: string;
+  token1: string;
+  fee: number;
+  tickSpacing: number;
+  maxLiquidityPerTick: ethers.BigNumber;
+}
+
+interface State {
+  liquidity: ethers.BigNumber;
+  sqrtPriceX96: ethers.BigNumber;
+  tick: number;
+  observationIndex: number;
+  observationCardinality: number;
+  observationCardinalityNext: number;
+  feeProtocol: number;
+  unlocked: boolean;
+}
+const UniswapTokenPrice = (props: any) => {
+  const [token1Price, setToken1Price] = useState("");
+  const [token2Price, setToken2Price] = useState("");
+  const poolContract = new ethers.Contract(
+    props.poolInfo.poolAdd,
+    IUniswapV3PoolABI,
+    props.provider
+  );
+  async function getPoolImmutables() {
+    const [factory, token0, token1, fee, tickSpacing, maxLiquidityPerTick] =
+      await Promise.all([
+        poolContract.factory(),
+        poolContract.token0(),
+        poolContract.token1(),
+        poolContract.fee(),
+        poolContract.tickSpacing(),
+        poolContract.maxLiquidityPerTick(),
+      ]);
+
+    const immutables: Immutables = {
+      factory,
+      token0,
+      token1,
+      fee,
+      tickSpacing,
+      maxLiquidityPerTick,
+    };
+    return immutables;
+  }
+
+  async function getPoolState() {
+    const [liquidity, slot] = await Promise.all([
+      poolContract.liquidity(),
+      poolContract.slot0(),
+    ]);
+
+    const PoolState: State = {
+      liquidity,
+      sqrtPriceX96: slot[0],
+      tick: slot[1],
+      observationIndex: slot[2],
+      observationCardinality: slot[3],
+      observationCardinalityNext: slot[4],
+      feeProtocol: slot[5],
+      unlocked: slot[6],
+    };
+
+    return PoolState;
+  }
+
+  useEffect(() => {
+    async function main() {
+      const { chainId } = await props.provider.getNetwork();
+
+      const [immutables, state] = await Promise.all([
+        getPoolImmutables(),
+        getPoolState(),
+      ]);
+      const TokenA = new Token(
+        chainId,
+        immutables.token0,
+        16,
+        props.poolInfo.token1,
+        props.poolInfo.token1
+      );
+
+      const TokenB = new Token(
+        chainId,
+        immutables.token1,
+        6,
+        props.poolInfo.token2,
+        props.poolInfo.token2
+      );
+
+      const poolExample = new Pool(
+        TokenA,
+        TokenB,
+        immutables.fee,
+        state.sqrtPriceX96.toString(),
+        state.liquidity.toString(),
+        state.tick
+      );
+
+      console.log(poolExample.token0Price.toSignificant());
+      const k = Number(state.sqrtPriceX96) ** 2 / 2 ** 192;
+      const kk = 2 ** 192 / Number(state.sqrtPriceX96) ** 2;
+      setToken1Price(poolExample.token0Price.toString());
+      setToken2Price(poolExample.token1Price.toString());
+      console.log(k, kk);
+    }
+    main();
+  });
+
+  return (
+    <div className="flex flex-column">
+      <div>
+        {props.poolInfo.token1} {token2Price}
+      </div>
+      <div>
+        {props.poolInfo.token2} {token1Price}
+      </div>
+    </div>
+  );
+};
+
+export default UniswapTokenPrice;
